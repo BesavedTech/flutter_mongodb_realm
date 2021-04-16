b1a1359678ff20e1d06aa0e0ab12cec55c59345b
BesavedTech <78008731+BesavedTech@users.noreply.github.com>
2021-04-16 10:04:43 +0000

    added custom data

--- lib/auth/core_realm_user.dart
+++ lib/auth/core_realm_user.dart
@@ -26,7 +26,7 @@ class CoreRealmUser {
         : CoreRealmUser(
             id: map["id"],
             deviceId: map["device_id"],
-            customData: map["customData"],
+            customData: UserProfile.fromMap(map['customData'] ?? Map())),
             profile: StitchUserProfile.fromMap(map['profile'] ?? Map()));
     
   }
